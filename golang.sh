2083adc2bc0a7ab2478d0a62762259066ec65704
mohdnansari <mohdnansari@gmail.com>
2018-12-19 23:21:35 +0530

    First commit
    First commit

---
+++ golang.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+
+curl -O https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
+
+tar -xvf go1.11.2.linux-amd64.tar.gz
+
+sudo mv go /go
+
+echo "export GOPATH=$HOME/go" >> ~/.profile
+echo "export PATH=$PATH:$GOPATH/bin" >> ~/.profile
+
+source ~/.profile
+
+
+go version
