83423b2cb52a1c494db3b4ee81d0f91581b0c4a2
mohdnansari <45333298+mohdnansari@users.noreply.github.com>
2018-12-25 15:35:26 +0530

    corrected typo
    removed unneeded lines

--- adddockeruser.sh
+++ adddockeruser.sh
@@ -2,7 +2,3 @@ sudo groupadd docker
 
 sudo usermod -aG docker $USER 
 /* $USER resolves to your current user on the ubuntu command line. you can find this out with the command whoami*/
-
-logout and login back
-
-docker run hello-world
